916d20aab49e1b5e5886edadce7a0187a929c8af
Jan_Lebar <janstefanlebar@gmail.com>
2023-03-28 12:17:32 +0200

    testing

---
+++ components/OptionsTemplate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function OptionsTemplate() {
+  return (
+    <div>
+      <h2>Options Template</h2>
+      <p>This is the template for the Options page.</p>
+    </div>
+  );
+}
+
+export default OptionsTemplate;
