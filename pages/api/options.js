f38e9f7b33cf2cf00389791decfc4eddeb064e0c
Jan_Lebar <janstefanlebar@gmail.com>
2023-03-28 12:02:25 +0200

    test

---
+++ pages/api/options.js
@@ -0,0 +1,10 @@
+import OptionsTemplate from '../components/OptionsTemplate';
+
+export default function Options() {
+  return (
+    <div>
+      <h1>Options</h1>
+      <OptionsTemplate />
+    </div>
+  );
+}
