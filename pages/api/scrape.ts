fa5eb72af34236cdd21e82c5146df0df710af380
Jan_Lebar <janstefanlebar@gmail.com>
2023-04-05 12:48:46 +0200

    update

--- pages/api/scrape.ts
+++ pages/api/scrape.ts
@@ -13,34 +13,29 @@
 //   const description = $('meta[name="description"]').attr('content')
 //   res.status(200).json({ title, description })
 // }
-// import fetch from 'isomorphic-unfetch';
-// import cheerio from 'cheerio';
-// import axios from 'axios';
-// import { NextApiRequest, NextApiResponse } from 'next';
 
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   const { url } = req.query;
-//   const { data } = await axios.get(url);
-//   const $ = cheerio.load(data);
-//   const title = $('title').text();
-//   res.status(200).send(title);
-// }
+import cheerio from 'cheerio';
+import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-import cheerio from 'cheerio'
-import axios from 'axios'
-import { NextApiRequest, NextApiResponse } from 'next'
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { url } = req.query;
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { url } = req.query
-  const { data } = await axios.get(url)
-  const $ = cheerio.load(data)
-  const title = $('title').text()
-  const description = $('meta[name="description"]').attr('content')
-  res.status(200).json({ title, description })
+  try {
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+
+    // Scrape the title
+    const title = $('title').text();
+
+    // Scrape the meta description
+    const description = $('meta[name="description"]').attr('content');
+
+    // Scrape the entire HTML content
+    const html = $('html').html();
+
+    res.status(200).json({ title, description, html });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
