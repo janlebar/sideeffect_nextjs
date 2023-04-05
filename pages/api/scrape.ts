652a619c67d1bb822e66297318861157ecf28d49
Jan_Lebar <janstefanlebar@gmail.com>
2023-04-05 12:41:16 +0200

    form for scrape works

--- pages/api/scrape.ts
+++ pages/api/scrape.ts
@@ -13,18 +13,34 @@
 //   const description = $('meta[name="description"]').attr('content')
 //   res.status(200).json({ title, description })
 // }
-import fetch from 'isomorphic-unfetch';
-import cheerio from 'cheerio';
-import axios from 'axios';
-import { NextApiRequest, NextApiResponse } from 'next';
+// import fetch from 'isomorphic-unfetch';
+// import cheerio from 'cheerio';
+// import axios from 'axios';
+// import { NextApiRequest, NextApiResponse } from 'next';
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   const { url } = req.query;
+//   const { data } = await axios.get(url);
+//   const $ = cheerio.load(data);
+//   const title = $('title').text();
+//   res.status(200).send(title);
+// }
+
+import cheerio from 'cheerio'
+import axios from 'axios'
+import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { url } = req.query;
-  const { data } = await axios.get(url);
-  const $ = cheerio.load(data);
-  const title = $('title').text();
-  res.status(200).send(title);
+  const { url } = req.query
+  const { data } = await axios.get(url)
+  const $ = cheerio.load(data)
+  const title = $('title').text()
+  const description = $('meta[name="description"]').attr('content')
+  res.status(200).json({ title, description })
 }
--- pages/scrape.js
+++ pages/scrape.js
@@ -83,36 +83,76 @@
 
 // export default PrescriptionForm;
 
-import React, { useState } from 'react';
-import { useRouter } from 'next/router';
+// import React, { useState } from 'react';
+// import { useRouter } from 'next/router';
 
-function PrescriptionForm() {
-  const [prescriptionOne, setPrescriptionOne] = useState('');
+// function PrescriptionForm() {
+//   const [prescriptionOne, setPrescriptionOne] = useState('');
 
-  const router = useRouter();
+//   const router = useRouter();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const url = `https://www.drugs.com/${prescriptionOne}.html`;
-    const response = await fetch(`/api/scrape?url=${url}`);
-    const scrapedData = await response.text();
-    // submit the form data to the server or perform some other action
-    router.push(`/prescriptions/${prescriptionOne}`);
-  };
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     const url = `https://www.drugs.com/${prescriptionOne}.html`;
+//     const response = await fetch(`/api/scrape?url=${url}`);
+//     const scrapedData = await response.text();
+//     // submit the form data to the server or perform some other action
+//     router.push(`/prescriptions/${prescriptionOne}`);
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <label htmlFor="prescriptionOne">Prescription One:</label>
+//       <input
+//         type="text"
+//         id="prescriptionOne"
+//         value={prescriptionOne}
+//         onChange={(e) => setPrescriptionOne(e.target.value)}
+//       />
+
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
+
+// export default PrescriptionForm;
+
+import { useState } from 'react'
+
+export default function MyComponent() {
+  const [data, setData] = useState(null)
+  const [urlInput, setUrlInput] = useState('aspirin.html')
+
+  const handleScrape = async () => {
+    const response = await fetch(`/api/scrape?url=https://www.drugs.com/${urlInput}#side-effects`)
+    const scrapedData = await response.json()
+    setData(scrapedData)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    handleScrape()
+  }
+
+  const handleChange = (event) => {
+    setUrlInput(event.target.value)
+  }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="prescriptionOne">Prescription One:</label>
-      <input
-        type="text"
-        id="prescriptionOne"
-        value={prescriptionOne}
-        onChange={(e) => setPrescriptionOne(e.target.value)}
-      />
-
-      <button type="submit">Submit</button>
-    </form>
-  );
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter drug name:
+          <input type="text" value={urlInput} onChange={handleChange} />
+        </label>
+        <button type="submit">Scrape Data</button>
+      </form>
+      {data && (
+        <div>
+          <h2>{data.title}</h2>
+          <p>{data.description}</p>
+        </div>
+      )}
+    </div>
+  )
 }
-
-export default PrescriptionForm;
