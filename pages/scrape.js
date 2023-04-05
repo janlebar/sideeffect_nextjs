4b1994b5e761837369ccb4cea290d49eee635d5a
Jan_Lebar <janstefanlebar@gmail.com>
2023-04-05 11:33:30 +0200

    test

--- pages/scrape.js
+++ pages/scrape.js
@@ -22,21 +22,97 @@
 //   )
 // }
 
-import { useState } from 'react';
+// import { useState } from 'react';
 
-export default function MyComponent() {
-  const [html, setHtml] = useState('');
+// export default function MyComponent() {
+//   const [html, setHtml] = useState('');
 
-  const handleScrape = async () => {
-    const response = await fetch('/api/scrape?url=https://www.drugs.com/aspirin.html#side-effects');
+//   const handleScrape = async () => {
+//     const response = await fetch('/api/scrape?url=https://www.drugs.com/aspirin.html#side-effects');
+//     const scrapedData = await response.text();
+//     setHtml(scrapedData);
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={handleScrape}>Scrape Data</button>
+//       {html && <div dangerouslySetInnerHTML={{ __html: html }} />}
+//     </div>
+//   );
+// }
+
+// import React, { useState } from 'react';
+// import { useRouter } from 'next/router';
+
+// function PrescriptionForm() {
+//   const [prescriptionOne, setPrescriptionOne] = useState('');
+//   const [prescriptionTwo, setPrescriptionTwo] = useState('');
+//   const router = useRouter();
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     const url = `https://www.drugs.com/${prescriptionOne}-${prescriptionTwo}.html`;
+//     const response = await fetch(`/api/scrape?url=${url}`);
+//     const scrapedData = await response.text();
+//     // submit the form data to the server or perform some other action
+//     router.push(`/prescriptions/${prescriptionOne}/${prescriptionTwo}`);
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <label htmlFor="prescriptionOne">Prescription One:</label>
+//       <input
+//         type="text"
+//         id="prescriptionOne"
+//         value={prescriptionOne}
+//         onChange={(e) => setPrescriptionOne(e.target.value)}
+//       />
+
+//       <label htmlFor="prescriptionTwo">Prescription Two:</label>
+//       <input
+//         type="text"
+//         id="prescriptionTwo"
+//         value={prescriptionTwo}
+//         onChange={(e) => setPrescriptionTwo(e.target.value)}
+//       />
+
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
+
+// export default PrescriptionForm;
+
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+
+function PrescriptionForm() {
+  const [prescriptionOne, setPrescriptionOne] = useState('');
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const url = `https://www.drugs.com/${prescriptionOne}.html`;
+    const response = await fetch(`/api/scrape?url=${url}`);
     const scrapedData = await response.text();
-    setHtml(scrapedData);
+    // submit the form data to the server or perform some other action
+    router.push(`/prescriptions/${prescriptionOne}`);
   };
 
   return (
-    <div>
-      <button onClick={handleScrape}>Scrape Data</button>
-      {html && <div dangerouslySetInnerHTML={{ __html: html }} />}
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="prescriptionOne">Prescription One:</label>
+      <input
+        type="text"
+        id="prescriptionOne"
+        value={prescriptionOne}
+        onChange={(e) => setPrescriptionOne(e.target.value)}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
   );
 }
+
+export default PrescriptionForm;
