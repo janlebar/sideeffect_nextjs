92442c4a4dc9ace06e48b09273842c21991fc393
Jan_Lebar <janstefanlebar@gmail.com>
2023-04-05 12:32:45 +0200

    form works

--- pages/scrape.js
+++ pages/scrape.js
@@ -1,17 +1,57 @@
+// import { useState } from 'react'
+
+// export default function MyComponent() {
+//   const [data, setData] = useState(null)
+
+//   const handleScrape = async () => {
+//     const response = await fetch('/api/scrape?url=https://www.drugs.com/aspirin.html#side-effects')
+//     const scrapedData = await response.json()
+//     setData(scrapedData)
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={handleScrape}>Scrape Data</button>
+//       {data && (
+//         <div>
+//           <h2>{data.title}</h2>
+//           <p>{data.description}</p>
+//         </div>
+//       )}
+//     </div>
+//   )
+// }
+
 import { useState } from 'react'
 
 export default function MyComponent() {
   const [data, setData] = useState(null)
+  const [urlInput, setUrlInput] = useState('aspirin.html')
 
   const handleScrape = async () => {
-    const response = await fetch('/api/scrape?url=https://www.drugs.com/aspirin.html#side-effects')
+    const response = await fetch(`/api/scrape?url=https://www.drugs.com/${urlInput}#side-effects`)
     const scrapedData = await response.json()
     setData(scrapedData)
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    handleScrape()
+  }
+
+  const handleChange = (event) => {
+    setUrlInput(event.target.value)
+  }
+
   return (
     <div>
-      <button onClick={handleScrape}>Scrape Data</button>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter drug name:
+          <input type="text" value={urlInput} onChange={handleChange} />
+        </label>
+        <button type="submit">Scrape Data</button>
+      </form>
       {data && (
         <div>
           <h2>{data.title}</h2>
