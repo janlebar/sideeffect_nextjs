a97877afb0a3bfc2b0ce9c35766846449ff41860
Jan_Lebar <janstefanlebar@gmail.com>
2023-05-04 17:21:05 +0200

    scrape titles works, scrape content needs fix

--- pages/api/scrape.ts
+++ pages/api/scrape.ts
@@ -60,22 +60,59 @@
 // }
 
 
-import cheerio from 'cheerio';
-import axios from 'axios';
-import { NextApiRequest, NextApiResponse } from 'next';
+//  vrne naslove
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { url } = req.query;
+// import cheerio from 'cheerio';
+// import axios from 'axios';
+// import { NextApiRequest, NextApiResponse } from 'next';
 
-  try {
-    const { data } = await axios.get(url);
-    const $ = cheerio.load(data);
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   const { url } = req.query;
+
+//   try {
+//     const { data } = await axios.get(url);
+//     const $ = cheerio.load(data);
+
+//     // Scrape all h2 titles
+//     const html = $('h2').map((i, el) => $(el).text()).get();
+
+//     res.status(200).json({ html });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// }
+
+
+import cheerio from 'cheerio'; // Importing the cheerio library which is used to parse HTML and XML documents.
+
+import axios from 'axios'; // Importing the axios library which is used to make HTTP requests.
+
+import { NextApiRequest, NextApiResponse } from 'next'; // Importing the Next.js API Request and Response interfaces.
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) { // Defining the API endpoint handler function.
+  const { url } = req.query; // Destructuring the "url" query parameter from the request object.
+
+  try { // Wrapping the code in a try-catch block to handle any errors that may occur.
+    const { data } = await axios.get(url); // Making an HTTP GET request to the URL specified in the "url" query parameter.
+    const $ = cheerio.load(data); // Parsing the HTML content using cheerio.
 
     // Scrape all h2 titles
-    const html = $('h2').map((i, el) => $(el).text()).get();
+    const h2Titles = $('h2').map((i, el) => $(el).text()).get(); // Extracting all the h2 tag titles.
 
-    res.status(200).json({ html });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+    // Scrape everything under the h2 titles
+    const h2Content = $('h2').map((i, el) => {
+      const title = $(el).text(); // Getting the h2 tag title.
+      const content = $(el).nextUntil('h2').map((j, e) => $(e).html()).get(); // Getting all the content under the h2 tag until the next h2 tag is encountered.
+      return { title, content };
+    }).get();
+
+    // const html = h2Titles.join('') + h2Content.join('');
+
+    res.status(200).json({ h2Titles, h2Content }); // Sending a JSON response containing the extracted h2 titles and their corresponding content.
+  } catch (error) { // Catching any errors that may have occurred.
+    res.status(500).json({ error: error.message }); // Sending a JSON response with an error message.
   }
 }
+
+
+
--- pages/scrape.js
+++ pages/scrape.js
@@ -1,73 +1,4 @@
 
-// import { useState } from 'react'
-
-// export default function MyComponent() {
-//   const [data, setData] = useState([])
-//   const [urlInputs, setUrlInputs] = useState([''])
-//   const [html, setHtml] = useState('')
-
-//   const handleScrape = async (urlInput) => {
-//     const response = await fetch(`/api/scrape?url=https://www.drugs.com/${urlInput}#side-effects`)
-//     const scrapedData = await response.json()
-//     setData((prevData) => [...prevData, scrapedData])
-//     setHtml(scrapedData.html)
-//   }
-
-//   const handleSubmit = (event, index) => {
-//     event.preventDefault()
-//     handleScrape(urlInputs[index])
-//   }
-
-//   const handleChange = (event, index) => {
-//     const newUrlInputs = [...urlInputs]
-//     newUrlInputs[index] = event.target.value
-//     setUrlInputs(newUrlInputs)
-//   }
-
-//   const handleAddInput = () => {
-//     setUrlInputs((prevInputs) => [...prevInputs, ''])
-//   }
-
-//   return (
-//     <div>
-//       <form onSubmit={(event) => handleSubmit(event, 0)}>
-//         <label>
-//           Enter drug name:
-//           <input type="text" value={urlInputs[0]} onChange={(event) => handleChange(event, 0)} />
-//         </label>
-//         <button type="submit">Scrape Data</button>
-//       </form>
-//       {data[0] && (
-//         <div>
-//           <h2>{data[0].title}</h2>
-//           <p>{data[0].description}</p>
-//           <div dangerouslySetInnerHTML={{ __html: html }}></div>
-//         </div>
-//       )}
-//       {urlInputs.slice(1).map((urlInput, index) => (
-//         <form key={index} onSubmit={(event) => handleSubmit(event, index + 1)}>
-//           <label>
-//             Enter drug name:
-//             <input type="text" value={urlInput} onChange={(event) => handleChange(event, index + 1)} />
-//           </label>
-//           <button type="submit">Scrape Data</button>
-//         </form>
-//       ))}
-//       <button onClick={handleAddInput}>Add Input</button>
-//       {data.slice(1).map((scrapedData, index) => (
-//         <div key={index}>
-//           <h2>{scrapedData.title}</h2>
-//           <p>{scrapedData.description}</p>
-//           <div dangerouslySetInnerHTML={{ __html: scrapedData.html }}></div>
-//         </div>
-//       ))}
-//     </div>
-//   )
-// }
-
-
-
-
 import React, { useState } from 'react';
 import Userinput from '../components/Userinput';
 // import './styles/scrape.css';
@@ -109,6 +40,7 @@ export default function MyComponent() {
     </div>
   );
 }
+
 
 
 // import React, { useState } from 'react';
