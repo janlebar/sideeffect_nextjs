7b7e5597de687b4c4bc54c989c98421ca5fb3449
Jan_Lebar <janstefanlebar@gmail.com>
2023-05-29 17:18:56 +0200

    using state error ;(

--- components/chart-color-scheme.js
+++ components/chart-color-scheme.js
@@ -7,3 +7,28 @@ export const getRandomColor = () => {
     return color;
   };
   
+
+
+//   import React, { useState, useEffect} from 'react';
+
+// export const getRandomColor = () => {
+//   const letters = "0123456789ABCDEF";
+//   let color = "#";
+//   for (let i = 0; i < 6; i++) {
+//     color += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return color;
+// };
+
+// const ExportedColor = () => {
+//   const [exportedColor, setExportedColor] = useState(null);
+
+//   useEffect(() => {
+//     const color = getRandomColor();
+//     setExportedColor(color);
+//   }, []);
+
+//   return exportedColor; // Return the generated color here
+// };
+
+// export default ExportedColor;
