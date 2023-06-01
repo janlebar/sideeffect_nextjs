84af2f0634d08161389a9dfd90de3d3ceb4a443e
Jan_Lebar <janstefanlebar@gmail.com>
2023-06-01 13:38:51 +0200

    change in componentsfrom

--- components/componentsfrom/chartold.js
+++ components/componentsfrom/chartold.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
-import { getRandomColor } from './chart-color-scheme';
 
 function RadarChart(args) {
-  const chartRef = useRef(null);
+  const chartRef = useRef();
   let chart = null;
 
   useEffect(() => {
@@ -16,77 +15,33 @@ function RadarChart(args) {
   console.log(args.data);
 
   const buildChart = () => {
-    if (chartRef.current) {
-      const myChartRef = chartRef.current.getContext('2d');
-      if (chart) {
-        destroyChart();
-      }
-
-      const categories = new Set(args.data.map((data) => data.category));
-      const categoryData = {};
-
-      // Initialize categoryData object with category as keys
-      categories.forEach((category) => {
-        categoryData[category] = { occurrence: 0, datasets: [] };
-      });
-
-      // Calculate total occurrence for each category and create datasets
-      args.data.forEach((data) => {
-        const category = data.category;
-        const occurrence = data.occurrence;
-
-        if (categoryData[category]) {
-          // Category already exists, add the occurrence to the existing value
-          categoryData[category].occurrence += occurrence;
-        } else {
-          // Category does not exist, create a new entry
-          categoryData[category] = { occurrence, datasets: [] };
-        }
-
-        categoryData[category].datasets.push({
-          label: `Dataset ${data.id}`,
-          data: [occurrence],
-          backgroundColor: getRandomColor(),
-          borderColor: getRandomColor(),
-          borderWidth: 1,
-        });
-      });
-
-      const datasets = [];
-
-      // Combine occurrence for each category and create stacked dataset
-      Object.keys(categoryData).forEach((category) => {
-        const { occurrence, datasets: categoryDatasets } = categoryData[category];
-
-        datasets.push({
-          label: `Category ${category}`,
-          data: [occurrence],
-          backgroundColor: getRandomColor(),
-          borderColor: getRandomColor(),
-          borderWidth: 1,
-          stack: category, // Assign the category as the stack to stack datasets together
-          categoryDatasets, // Store individual datasets for later reference
-        });
-      });
-
-      chart = new Chart(myChartRef, {
-        type: 'radar',
-        data: {
-          labels: Array.from(categories),
-          datasets: datasets,
-        },
-        options: {
-          scales: {
-            r: {
-              ticks: {
-                beginAtZero: true,
-                max: 10,
-              },
-            },
-          },
-        },
-      });
+    const myChartRef = chartRef.current.getContext('2d');
+    if (chart) {
+      destroyChart();
     }
+    chart = new Chart(myChartRef, {
+      type: 'radar',
+      data: {
+        labels: ['Label 1', 'Label 2', 'Label 3', 'Label 4', 'Label 5'],
+        datasets: [{
+          label: 'Dataset 1',
+          data: [3, 5, 2, 6, 7],
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          r: {
+            ticks: {
+              beginAtZero: true,
+              max: 10
+            }
+          }
+        }
+      }
+    });
   };
 
   const destroyChart = () => {
@@ -96,7 +51,9 @@ function RadarChart(args) {
     }
   };
 
-  return <canvas ref={chartRef}></canvas>;
+  return (
+    <canvas ref={chartRef} />
+  );
 }
 
 export default RadarChart;
