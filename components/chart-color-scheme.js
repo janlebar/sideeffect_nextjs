c3af4691bf6eba88067816905aeeaadeade135e0
Jan_Lebar <janstefanlebar@gmail.com>
2023-06-21 20:33:05 +0200

    radarchart demo works transparencies added to chart-color-scheme

--- components/chart-color-scheme.js
+++ components/chart-color-scheme.js
@@ -1,11 +1,13 @@
-export const getRandomColor = () => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
+export const getRandomColor = (alpha) => {
+  const letters = '0123456789ABCDEF';
+  let color = 'rgba(';
+  for (let i = 0; i < 3; i++) {
+    color += Math.floor(Math.random() * 256) + ',';
+  }
+  color += alpha + ')';
+  return color;
+};
+
   
 
 //   import React, { useState, useEffect} from 'react';
--- components/radarchart.js
+++ components/radarchart.js
@@ -1,49 +1,78 @@
-import React, { useEffect, useRef } from 'react';
-import Chart from 'chart.js/auto';
+import { useEffect, useRef } from 'react';
+import { Chart } from 'chart.js/auto';
+import { getRandomColor } from './chart-color-scheme';
 
 const RadarChartTwo = () => {
-  const chartRef = useRef(null);
-  let chartInstance = null;
 
-  useEffect(() => {
-    const labels = ['Label 1', 'Label 2', 'Label 3', 'Label 4'];
-    const data = [3, 5, 2, 7]; // Example data values for the labels
-
-    const ctx = chartRef.current.getContext('2d');
-
-    if (chartInstance) {
-      chartInstance.destroy();
-    }
-
-    chartInstance = new Chart(ctx, {
-      type: 'radar',
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            label: 'Dataset',
-            data: data,
-            backgroundColor: 'rgba(75, 192, 192, 0.5)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            pointBackgroundColor: 'rgba(75, 192, 192, 1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(75, 192, 192, 1)',
+      const chartRef = useRef(null);
+      const chartInstanceRef = useRef(null);
+    
+      useEffect(() => {
+        const config = {
+          type: 'radar',
+          data: {
+            labels: [
+              'Eating',
+              'Drinking',
+              'Sleeping',
+              'Designing',
+              'Coding',
+              'Cycling',
+              'Running'
+            ],
+            datasets: [{
+              label: 'My First Dataset',
+              data: [65, 59, 90, 81, 56, 55, 40],
+              fill: true,
+              backgroundColor: getRandomColor(0.2),
+              borderColor: getRandomColor(0.2),
+              pointBackgroundColor: getRandomColor(),
+              pointBorderColor: getRandomColor(),
+              pointHoverBackgroundColor:  getRandomColor(),
+              pointHoverBorderColor:  getRandomColor(),
+            }, {
+              label: 'My Second Dataset',
+              data: [28, 48, 40, 19, 96, 27, 100],
+              fill: true,
+              backgroundColor: getRandomColor(0.2),
+              borderColor: getRandomColor(0.2),
+              pointBackgroundColor: getRandomColor(),
+              pointBorderColor:  getRandomColor(),
+              pointHoverBackgroundColor:  getRandomColor(),
+              pointHoverBorderColor:  getRandomColor(),
+            }]
           },
-        ],
-      },
-      options: {
-        scales: {
-          r: {
-            beginAtZero: true,
-            max: 10,
-          },
-        },
-      },
-    });
-  }, []);
+          options: {
+            elements: {
+              line: {
+                borderWidth: 3
+              }
+            }
+          }
+        };
+    
+        const ctx = chartRef.current.getContext('2d');
+    
+        // Destroy the existing chart instance if it exists
+        if (chartInstanceRef.current) {
+          chartInstanceRef.current.destroy();
+        }
+    
+        // Create a new chart instance
+        chartInstanceRef.current = new Chart(ctx, config);
+    
+        // Clean up the chart instance when the component unmounts
+        return () => {
+          if (chartInstanceRef.current) {
+            chartInstanceRef.current.destroy();
+            chartInstanceRef.current = null;
+          }
+        };
+      }, []);
+    
+      return <canvas ref={chartRef} />;
+    };
 
-  return <canvas ref={chartRef} />;
-};
+    
 
 export default RadarChartTwo;
