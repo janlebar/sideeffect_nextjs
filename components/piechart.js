90c11cbe1c80287955a0481e225d7618a5006f57
Jan_Lebar <janstefanlebar@gmail.com>
2023-07-24 13:24:50 +0200

    update

--- components/piechart.js
+++ components/piechart.js
@@ -1,40 +1,47 @@
-
-
 import React, { useEffect, useRef } from 'react';
 import Chart from 'chart.js/auto';
 import { getRandomColor } from './chart-color-scheme';
 
 function PieChart({ data }) {
+  // Create a reference to the canvas element for the chart
   const chartRef = useRef(null);
-  let chart = null;
+  let chart = null; // Initialize a variable to hold the Chart.js instance
 
+  // This useEffect hook will run whenever the 'data' prop changes
   useEffect(() => {
-    buildChart();
+    buildChart(); // Build the chart when the component mounts or when 'data' prop changes
     return () => {
-      destroyChart();
+      destroyChart(); // Destroy the chart when the component unmounts to prevent memory leaks
     };
   }, [data]);
 
+
+
+  
   const buildChart = () => {
-    if (!chartRef.current) return;
-    const myChartRef = chartRef.current.getContext('2d');
+    if (!chartRef.current) return; // If the canvas element is not available, return early
+
+    const myChartRef = chartRef.current.getContext('2d'); // Get the 2D context of the canvas
 
     if (chart) {
-      destroyChart();
+      destroyChart(); // If there is an existing chart instance, destroy it first
     }
-    
+
     const categories = new Set();
+    // Collect all unique categories from the 'data' array of symptoms
     for (const symptoms of data) {
       for (const symptom of symptoms) {
         categories.add(symptom.category);
       }
     }
-    
+
     const datasets = [];
+    // Create datasets for each set of symptoms (presumably for different medicines) in 'data'
     for (const symptoms of data) {
       const medicineName = symptoms[0].medicine;
 
       const occurrences = {};
+      // Calculate the occurrences of each category for a specific set of symptoms (medicine)
       for (const symptom of symptoms) {
         for (const category of categories) {
           if (category in occurrences) continue;
@@ -47,33 +54,35 @@ function PieChart({ data }) {
           }
         }
       }
-      
+
+      // Add a dataset for the medicine with its occurrences for each category
       datasets.push({
         label: `Niz podatkov ${medicineName}`,
         data: Object.values(occurrences),
-        backgroundColor: Array.from(categories).map(() => getRandomColor(0.2)),
-        borderColor: Array.from(categories).map(() => getRandomColor(0.2)),
+        backgroundColor: Array.from(categories).map(() => getRandomColor(0.2)), // Generate random colors for the pie chart slices
+        borderColor: Array.from(categories).map(() => getRandomColor(0.2)), // Generate random colors for the pie chart borders
         borderWidth: 1,
       });
     }
-    
+
+    // Create a new Chart.js instance with the gathered data
     chart = new Chart(myChartRef, {
-      type: 'pie',
+      type: 'pie', // Chart type is pie
       data: {
-        labels: Array.from(categories),
-        datasets: datasets,
+        labels: Array.from(categories), // The labels for the pie chart slices (category names)
+        datasets: datasets, // The data and settings for each dataset (medicine)
       },
     });
   };
 
   const destroyChart = () => {
     if (chart) {
-      chart.destroy();
-      chart = null;
+      chart.destroy(); // Destroy the existing Chart.js instance when unmounting the component
+      chart = null; // Reset the chart variable
     }
   };
 
-  return <canvas ref={chartRef}></canvas>;
+  return <canvas ref={chartRef}></canvas>; // Render the canvas element that will hold the chart
 }
 
 export default PieChart;
--- pages/index.tsx
+++ pages/index.tsx
@@ -70,20 +70,7 @@ const Home = () => {
             className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
             style={{ height: "80px", transform: "translateZ(0)" }}
           >
-            <svg
-              className="absolute bottom-0 overflow-hidden"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0"
-            >
-              <polygon
-                className="text-white fill-current"
-                points="2560 0 2560 100 0 100"
-              ></polygon>
-            </svg>
+
           </div>
           <div className="container mx-auto px-4">
             <div className="items-center flex flex-wrap">
