2cfceb10ba8bfa14c67d7d1c43a5cb477ddc8a3b
Jan_Lebar <janstefanlebar@gmail.com>
2023-09-03 18:55:00 +0200

    fix index

--- pages/index.js
+++ pages/index.js
@@ -147,16 +147,7 @@ export default function MyMergedComponent() {
           <div className="container mx-auto px-4">
             <div className="flex flex-wrap">
               <div className="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
-                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
-                  <div className="px-4 py-5 flex-auto">
-                  <div className="p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full" style={{ background: 'linear-gradient(to right, white 50%, #0074e4 50%)', clipPath: 'ellipse(100% 50%)' }}>
-                      <i className="fas fa-award"></i>
-                    </div>
-                    <p className="mt-2 mb-4 text-gray-600 ml-5">
-                      +
-                    </p>
-                  </div>
-                </div>
+                
               </div>
               {/* Add more service cards */}
             </div>
@@ -171,16 +162,14 @@ export default function MyMergedComponent() {
 
 
 {/* Contact Section */}
-<section className="relative block py-24 lg:pt-0 bg-gray-900">
+<section className=" z-10">
   <div className="container mx-auto px-4">
     <div className="flex flex-wrap justify-center lg:-mt-100 -mt-100">
       <div className="w-full lg:w-6/12 px-4">
         <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300">
           <div className="flex-auto p-5 lg:p-10">
-            <h4 className="text-2xl font-semibold">Want to work with us?</h4>
-            <p className="leading-relaxed mt-1 mb-4 text-gray-600">
-              Complete this form and we will get back to you in 24 hours.
-            </p>
+
+
             <div className="relative w-full mb-3 mt-8">
               <label
                 className="block uppercase text-gray-700 text-xs font-bold mb-2"
