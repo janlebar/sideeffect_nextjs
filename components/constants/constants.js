4559a9d11e37349846faf46c96ad24771d03d9da
Jan_Lebar <janstefanlebar@gmail.com>
2023-10-16 17:45:33 +0200

    cleaning the code

---
+++ components/constants/constants.js
@@ -0,0 +1,20 @@
+const color = [
+    'rgba(255, 255, 0, 0.5)',       // Safety Yellow
+    'rgba(255, 0, 0, 0.5)',         // Bright Red
+    'rgba(51, 153, 255, 0.5)',      // High-Visibility Blue
+    'rgba(204, 255, 102, 0.5)',     // Fluorescent Green
+    'rgba(0, 153, 204, 0.5)',       // Electric Blue
+    'rgba(255, 204, 0, 0.5)',       // Day-Glo Yellow
+    'rgba(0, 255, 0, 0.5)',         // Neon Green
+    'rgba(204, 0, 0, 0.5)',         // Signal Red
+    'rgba(204, 255, 0, 0.5)',       // Fluo Yellow-Green
+    'rgba(204, 0, 0, 0.5)',         // Warning Red
+    'rgba(153, 51, 255, 0.5)',      // Vibrant Purple
+    'rgba(255, 0, 255, 0.5)',       // Magenta
+    'rgba(0, 204, 102, 0.5)',       // Emerald Green
+    'rgba(153, 0, 204, 0.5)',       // Electric Purple
+    'rgba(204, 204, 0, 0.5)',       // Neon Yellow
+    'rgba(102, 204, 255, 0.5)',     // Sky Blue
+    'rgba(0, 102, 204, 0.5)',       // Cornflower Blue
+    'rgba(0, 204, 255, 0.5)'        // Aqua Blue
+  ];
